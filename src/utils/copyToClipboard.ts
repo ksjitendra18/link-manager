7a84f3c154487e80cbd224c43d96af47873ee502
Jitendra <jitendraks1803@gmail.com>
2023-02-05 10:50:45 +0530

    added toast

--- src/utils/copyToClipboard.ts
+++ src/utils/copyToClipboard.ts
@@ -1,5 +1,10 @@
+import { toast } from "react-toastify";
 const copyToClipboard = async (text: string) => {
   //   if ("clipboard" in navigator) {
+  toast.success("Copied Successfully", {
+    hideProgressBar: true,
+    autoClose: 800,
+  });
   return await navigator.clipboard.writeText(text);
   //   } else {
   //     return document.execCommand("copy", true, text);
